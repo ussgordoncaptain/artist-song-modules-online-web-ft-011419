79598bdade1843d622a3531fc1cfef65bb260cb2
Edmund Nelson <edmund.nelson.b@gmail.com>
2019-02-20 21:19:34 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,5 +1,5 @@
 require 'pry'
-
+require_relative '../lib/Concerns/memorable'
 class Artist
   attr_accessor :name
   attr_reader :songs
@@ -19,13 +19,13 @@ class Artist
     @@artists
   end
 
-  def self.reset_all
-    self.all.clear
-  end
+ # def self.reset_all
+   # self.all.clear
+ #end
 
-  def self.count
-    self.all.count
-  end
+ # def self.count
+ #   self.all.count
+ # end
 
   def add_song(song)
     @songs << song
